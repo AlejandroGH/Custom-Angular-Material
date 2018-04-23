1c034e83dbef44bc663e77eb3e7e96c27f22b47c
Alejandro Garcia <alejandro.garcia@navis.com>
2018-04-23 15:56:23 +0200

    new styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
+import { MatButtonModule} from '@angular/material/button';
+import { MatSelectModule} from '@angular/material/select';
 
 
 
@@ -18,6 +19,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    NoopAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
